7b3996e1a10673fe11ca5df5fca32865e1f53ba0
Ronaldo Ramos <ronaldo-ramos@hotmail.com>
2022-12-11 11:29:17 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -9,5 +9,26 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>(0);
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(Seller => Seller.TotalSales(initial, final));
+        }
     }
 }
